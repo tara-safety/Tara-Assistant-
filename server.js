69cfeb3a896fea13b1400ba7610f3134b8859da7
tara-safety <safetybuzzalerts@gmail.com>
2026-03-08 20:32:34 -0300

    Update server.js

--- server.js
+++ server.js
@@ -265,7 +265,7 @@ app.get("/simulate-impact", async (req, res) => {
    START SERVER
 -------------------------*/
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`TARA server running on port ${PORT}`);
