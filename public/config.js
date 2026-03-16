954e328f7d2cbc25276ef9c382442fe74d55b230
tara-safety <safetybuzzalerts@gmail.com>
2026-03-16 20:04:09 -0300

    Create config.js

---
+++ public/config.js
@@ -0,0 +1,10 @@
+export const DRIVER_NAME = "Tow Operator";
+export const COMPANY = "TARA Safety";
+
+export const IMPACT_LIMIT = 30;
+export const INACTIVITY_LIMIT = 8 * 60 * 1000;
+export const IMPACT_COOLDOWN = 8000;
+export const EMERGENCY_COUNTDOWN = 30;
+
+export const ALARM_URL =
+  "https://actions.google.com/sounds/v1/alarms/alarm_clock.ogg";
