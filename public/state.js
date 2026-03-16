6da8d1f484d17167b75408f8287adb745870d436
tara-safety <safetybuzzalerts@gmail.com>
2026-03-16 20:05:08 -0300

    Create state.js

---
+++ public/state.js
@@ -0,0 +1,15 @@
+export const state = {
+  voiceEnabled: true,
+  driverMinderActive: false,
+  towModeActive: false,
+  motionStarted: false,
+  emergencyActive: false,
+  emergencyRunning: false,
+  listeningForCommand: false,
+  systemUnlocked: false,
+  lastImpactTime: 0,
+  inactivityTimer: null,
+  holdTimer: null,
+  alarmAudio: null,
+  recognition: null
+};
